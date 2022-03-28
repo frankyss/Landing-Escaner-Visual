327aec6a21aa8a16d24c6446584f0a6231e29ddf
frankyss <92185478+frankyss@users.noreply.github.com>
2022-03-28 20:38:32 +0200

    Add files via upload

---
+++ Eye Scanner/script.js/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+    setTimeout(function() {
+        $(".btn").fadeIn(2500);
+    },6000);
+    
+    setTimeout(function() {
+        $(".btn").hide(0);
+    },0);
+   
+});
